6c763db52c8fd3bcc98fcbd9df4ec780393f1c79
Superxiongw <xiongwei1031@163.com>
2022-07-20 18:06:27 +0800

    first commit

---
+++ ApiCase/business/commons.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import requests
+from ApiCase.common.logger import logger
+from ApiCase.common.httpclient import HttpClient
+from ApiCase.common.yaml_util import YamlUtil
+
+# 获取Token----------------公共方法
+def get_token():
+    Token = YamlUtil().read_extract_yaml('Token')
+    return Token
+# 创建委托协议
+def CreateAgreement(udata,herders):
+    url= udata['url']
+    data = udata['data']
+    # 关闭https校验请求
+    requests.packages.urllib3.disable_warnings()
+    res = HttpClient().send_request(url=url, param_type=udata['parmams_type'], method=udata['method'], data=data,
+                                        headers=herders, verify=False)
+    logger.debug(f'新增委托协议信息：{res}')
+    return res
+
